fa082821be1494beb0fd5293c9dbdb45bde56fbc
RyanMcklain <piro27@gmail.com>
2019-10-06 21:05:24 +0300

    build

---
+++ build/precache-manifest.848461e7a7b8744d5cdff14e535c44fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12add168f1f02e406dcaef68c30625d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a095e1570b32ca6ae6c",
+    "url": "/static/css/main.e3c9702e.chunk.css"
+  },
+  {
+    "revision": "d7cf635560db0ec04e08",
+    "url": "/static/js/2.d92b289d.chunk.js"
+  },
+  {
+    "revision": "8a095e1570b32ca6ae6c",
+    "url": "/static/js/main.5b1fb8a5.chunk.js"
+  },
+  {
+    "revision": "f4b874c4dbcef367375b",
+    "url": "/static/js/runtime-main.84bfd413.js"
+  },
+  {
+    "revision": "3bc6722081bd1b8c3061f77ad2b741de",
+    "url": "/static/media/background-image.3bc67220.jpg"
+  },
+  {
+    "revision": "394e8e4d908d30cb3bde1617d25983e5",
+    "url": "/static/media/heroes2-1.394e8e4d.jpg"
+  },
+  {
+    "revision": "a3948d98233116456e9b81ba9f0acabb",
+    "url": "/static/media/semantics.a3948d98.jpg"
+  },
+  {
+    "revision": "d62f707e7371c66dac097e8dd932c269",
+    "url": "/static/media/sprites.d62f707e.svg"
+  },
+  {
+    "revision": "7722e0b341d8b461a2c4b20401cc7c16",
+    "url": "/static/media/tesla.7722e0b3.jpg"
+  },
+  {
+    "revision": "8dd9ad771c312c1dba83a0eb727384bd",
+    "url": "/static/media/that_guy.8dd9ad77.jpg"
+  },
+  {
+    "revision": "5e3c73fc6301a98e0fd6742d4485cb5b",
+    "url": "/static/media/whole_human.5e3c73fc.jpg"
+  }
+]);
--- src/components/header.js
+++ src/components/header.js
@@ -14,6 +14,28 @@ const titleStyle = {
 };
 
 export const Header = ({ articles, updateSearchResults }) => {
+  fetch("https://raw.githubusercontent.com/RyanMcklain/walak/master/src/mocks/culture-heroes.json", {
+    "credentials": "omit",
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+      "accept-language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "max-age=0",
+      "if-none-match": "\"c7eb0a4415061c5643231344d7fa63e523ee7e7b\"",
+      "sec-ch-ua": "Google Chrome 77",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "cross-site",
+      "sec-fetch-user": "?1",
+      "sec-origin-policy": "0",
+      "upgrade-insecure-requests": "1"
+    },
+    // "referrer": "https://github.com/RyanMcklain/walak/blob/master/src/mocks/culture-heroes.json",
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": null,
+    "method": "GET",
+    "mode": "cors"
+  });
+
   return (
     <div id="header" style={ headerStyle }>
       <div id="header-logo" className="link">
